3312a060812c904454fc6a9c8cf2c1d85035a8c7
DavidNdon <davidnjorogemesi@gmail.com>
2026-01-19 19:05:20 +0300

    object.js file is added

---
+++ object.js
@@ -0,0 +1,11 @@
+let person = {
+    name: 'John',
+    age: 30,
+    career: 'Software Engineer',
+    address:{
+        city: 'New York',
+        state: 'NY'
+    }
+}
+
+console.log(person.name+ " lives in "+ person.address.city);
